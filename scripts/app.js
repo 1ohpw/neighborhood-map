1e0fc32b39a0293b95cf15935cfdf13a64ea0aac
denzalereese <dlr249@cornell.edu>
2016-10-03 14:53:28 -0700

    map and listview setup

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
-console.log('Sanity check');
+$(document).ready(function() {
+  var map;
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 37.7749, lng: -122.4194},
+      zoom: 10,
+    });
+  }
+
+  initMap();
+});
